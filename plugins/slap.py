bf28041f63a3c68dd01b1e6dfe4aaaa3022e53a0
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2015-04-16 15:26:06 -0700

    Bot now uses the full user input as the name and slaps with a random object

--- plugins/slap.py
+++ plugins/slap.py
@@ -1,10 +1,25 @@
+from random import choice
+
+
 def slap(bot, event, *args):
 
     if not args:
 
         bot.send_message(event.conv, 'Who should I slap?')
+        return
 
-    txt = '/me slaps {} around a bit with a large black cock'
-    message = txt.format(args[0])
+    name = ' '.join(list(args))
+
+    objects = [
+        "a large trout",
+        "a large black cock",
+        "a soggy noodle",
+        "an iron gauntlet"
+    ]
+
+    message = '/me slaps {} around a bit with {}'.format(
+        name,
+        choice(objects)
+    )
 
     bot.send_message(event.conv, message)
