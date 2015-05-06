4af0e3cf2349b854d70d16d2962e70374a5f8468
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2015-05-06 10:46:26 -0700

    Added 'busy' command

---
+++ plugins/busy.py
@@ -0,0 +1,49 @@
+from random import choice
+from re import sub
+
+
+def busy(bot, event, *args):
+
+    verbs = [
+        'absorbing', 'adjusting', 'allocating', 'compiling',
+        'compressing', 'deallocating', 'decoding', 'decompliling',
+        'decompressing', 'decrypting', 'demultiplexing', 'disabling',
+        'enabling', 'encoding', 'encrypting', 'factoring',
+        'generating', 'indexing', 'initializing', 'mapping',
+        'multiplexing', 'parsing', 'prioritizing', 'reordering',
+        'resolving', 'reticulating', 'routing', 'sorting',
+        'transcoding', 'upgrading'
+    ]
+
+    adjectives = [
+        'active', 'associative', 'bi-directional', 'corrupt',
+        'complex', 'cybernetic', 'dank', 'deterministic',
+        'duplicate', 'dynamic', 'ethereal', 'euclidean',
+        'finite', 'high-level', 'infinie', 'inverse',
+        'linked', 'low-level', 'multi-dimensional', 'negative',
+        'non-euclidean', 'positive', 'prallel', 'quantifiable',
+        'random', 'sentient', 'sub-zero', 'tertiary',
+        'unlinked', 'unusual', 'well-documented', 'vectorized',
+        '' # Intentionally blank
+    ]
+
+    nouns = [
+        'archives', 'arrays', 'caches', 'coprocesses',
+        'cores', 'datasets', 'fields', 'frames',
+        'functions', 'datastores', 'lists', 'matrices',
+        'objects', 'procedures', 'processes', 'queues',
+        'receptacles', 'repositories', 'sectors', 'segments',
+        'sequences', 'splines', 'states', 'structures',
+        'tables', 'threads'
+    ]
+
+    message = '{} {} {}'.format(
+        choice(verbs),
+        choice(adjectives),
+        choice(nouns)
+    )
+
+    bot.send_message(
+        event.conv,
+        sub(' +', ' ', message)
+    )
